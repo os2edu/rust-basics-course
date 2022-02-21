afaa1ca333ae27cd318415a84c170aa2e73283ea
Li Ming <limingth@gmail.com>
2022-02-21 10:08:18 +0800

    Create Josephus-Ring rust code

---
+++ ring.rs
@@ -0,0 +1,38 @@
+const NUM: usize = 100;
+const COUNT: usize = 3;
+    
+fn main() {
+    let mut ring = [0; NUM];
+    for index in 0..NUM {
+        ring[index] = index + 1;
+        println!("ring[{}], {}", index, ring[index])
+    }
+    // return ();
+    let mut i = 0;
+    let mut j = 0;
+    let mut out = 0;
+    while out < NUM {
+        // println!("当前位置 {}, ring[i]={}", i+1, ring[i]);
+
+        if ring[i] != 0 {
+            j = j + 1;
+            println!("当前位置 {}, 报数 j = {}", i + 1, j);
+
+            if j == COUNT {
+                println!("{} --> out", i+1);
+                out = out + 1;
+                ring[i] = 0;
+                j = 0;
+            }
+      
+        }
+
+        i = (i + 1) % NUM;
+        // i = i + 1;
+        // if i == NUM {
+        //   i = 0;
+        // }
+    }
+      
+    println!("Josephus ring game is over!");
+}
